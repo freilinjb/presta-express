3776e31099ee762318e2e7c732619175cafa174f
freilinjb <freilinjb@gmail.com>
2020-05-14 23:09:47 -0400

    trabajando en sectores pero tengo un error

--- pages/Sectores.jsx
+++ pages/Sectores.jsx
@@ -1,92 +1,150 @@
-import React,{useState,useContext, useEffect} from "react";
+//FRONTAL DE CLIENTE
+import React, { useState, useContext, useEffect } from "react";
+import Link from 'next/link';
 import Layout from "../components/layout/Layout";
+import { FirebaseContext } from "../firebase";
 import Navegacion from "../components/layout/Navegacion";
-import InputModal from '../components/ui/InputModal';
 import Sector from '../components/ui/Sector';
-import {FirebaseContext} from '../firebase'; 
-
+import Spinner from '../components/ui/Spinner';
 const Sectores = () => {
-
   // const {sectores} = useSector("creado");
+  const [cargando, setCargando] = useState(false);
+  const [busqueda, setBusqueda] = useState('');
 
-  const [sectores, setSectores ] = useState([]);
-  const {firebase, usuario} = useContext(FirebaseContext);
+  const [sectores, setSectores] = useState([]);
+  const { firebase, usuario } = useContext(FirebaseContext);
 
-    // const usuario = useAutenticacion();
+  const handleChange =e=> {
+    setBusqueda(e.target.value);
+    // console.log(busqueda);
+  }
 
-    if(usuario) {
-      const { uid } = usuario;
-      const obtenerSectores =() => {  
-        firebase.db.collection("Sectores").where("creador.id","==",uid).orderBy("creado", 'desc').onSnapshot(manejarSnapshot);//Ordena por creado
-      }
-      obtenerSectores();
-    }
-  
-    // useEffect(() => {
-    //   //Esta funcion te da acceso a todos los datos
-    //   //y snapshot realiza operaciones con ellos
-    //   // console.log(usuario);
-    //   if(usuario){
-    //     console.log(usuario);
+
+
+  // const usuario = useAutenticacion();
+
+  useEffect(() => {
+    if(usuario && busqueda.trim() === '') {
+        const { uid } = usuario;
         
-       
-    //   }
-    // },[usuario]);
-    
-    //se ejecuta cuando el componente esta listo
-    function manejarSnapshot(snapshot) {
-      const sectores = snapshot.docs.map(doc => {
-        //Extrae todo el registro completo
-        return {
-          id: doc.id,
-          ...doc.data()
+        //Esta funcion te da acceso a todos los datos
+        //y snapshot realiza operaciones con ellos
+        try {
+          setCargando(true);
+
+          const obtenerClientes =() => {
+            firebase.db.collection("Clientes").where("creador.id","==",uid).orderBy("creado", 'desc').onSnapshot(manejarSnapshot);//Ordena por creado
+          }
+          obtenerClientes();
+        } catch (error) {
+          console.log(error);
         }
-      });
-   
-      //resultado de la consulta
-      setSectores(sectores);
+        finally {
+          setCargando(false);
+        }
     }
-    
-    // const {nombre, descripcion, id } = sector;
+  },[sectores, busqueda]);
+  //se ejecuta cuando el componente esta listo
+  function manejarSnapshot(snapshot) {
+    const sectores = snapshot.docs.map(doc => {
+      //Extrae todo el registro completo
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    });
+ 
+    //resultado de la consulta
+    setSectores(sectores);   
+    // console.log(clientes);
+     
+  }
+  
+  // console.log(clientes);
+  
+  const Componente = (cargando) ? <Spinner/> : 
+  
+    (<div className="col-lg-12">
+      <div className="section-block">
+          <h3 className="section-title">Lista de Sectores</h3>
+      </div>
+      <div className="card">
+	            <div className="campaign-table table-responsive">
+	                <table className="table">
+                  <thead>
+	                        <tr className="border-0">
+	                            <th className="border-0">Nombre</th>
+	                            <th className="border-0">Descripcion</th>
+	                            <th className="border-0">Accion</th>
+	                        </tr>
+	                    </thead>
+                      {sectores.map(sector => (
+                          <Sector
+                          key={sector.id}
+                          sector={sector}
+                          />
+                      ))}
+                </table>
+      </div>
+  </div></div>)
 
-    return (
-    <>
-      <Layout>
-      <Navegacion titulo={"Registro de Sectores"}>
-        <div className="dashboard-short-list">
-            <button type="button" className="btn btn-primary float-right shadow"  data-toggle="modal" data-target="#Modal">Registrar un sector</button>
-            <InputModal/>
-            
+    const hanbleBuscar =e=> {
+        e.preventDefault();
 
-          <div className="row justify-content-center">
-            {/* <!-- ============================================================== --> */}
-            {/* <!-- shortable list  --> */}
-            {/* <!-- ============================================================== --> */}
-            <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 co-12">
+        if(busqueda.trim()) {
+          const buscar = busqueda.toLowerCase().trim()
+          const filtro = sectores.filter(sector => {
+              return(
+                (sector.nombre.toLowerCase().includes(buscar) || sector.descripcion.toLowerCase().includes(buscar))
+              )
+          });
+          
+          //filtro itera en cada uno de ellos, combierte el nombrer en minusculas 
+          //y luego si lo encuentra lo agrega a filter
+          // console.log(filtro,' BUSQUEDA: ', busqueda);
+          setSectores(filtro);
+          // console.log(clientes);
+        }   
+    }
+  return (
+    <Layout>
+		{/* {firebase.cargando && (<div className="spinner"><Spinner/></div>)} */}
 
-              <section className="card card-fluid">
-                <h5 className="card-header drag-handle">Lista de Sectores </h5>
-                <ul
-                  className="sortable-lists list-group list-group-flush list-group-bordered"
-                  id="items"
-                >
-                  {sectores.map(sector => (
-                    <Sector
-                    key={sector.id}
-                    sector={sector}
-                    />
-                ))}
-                </ul>
-              </section>
+      <Navegacion titulo={"Lista de Clientes"}>
+      
+            <div className="row justify-content-center">
+                <Link href="/add/Cliente">            
+                    <a className="btn btn-primary shadow float-right col-md-auto offset-md-7">Registrar un Sectores</a>
+                </Link>
+                <div className="col-xl-8 col-lg-8 col-md-12 col-sm-12 mt-2">
+                    
+                    <div className="card">
+                        <div className="card-body">
+                            <form
+                              onSubmit={hanbleBuscar}
+                            >
+                              <input
+                                  className="form-control form-control-lg"
+                                  type="search"
+                                  placeholder="Buscar..."
+                                  aria-label="Buscar..."
+                                  name="busqueda"
+                                  value={busqueda}
+                                  onChange={handleChange}
+                              />
+                              <button className="btn btn-primary search-btn" type="submit">
+                                  Buscar
+                              </button>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="">
+                  { Componente } 
+                </div>
+                </div>
             </div>
-          </div>
-          {/* <!-- ============================================================== --> */}
-          {/* <!-- end nestable list  --> */}
-          {/* <!-- ============================================================== --> */}
-        </div>
-        </Navegacion>
-      </Layout>
-    </>
+      </Navegacion>
+    </Layout>
   );
 };
 
