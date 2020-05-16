11df4b4315740eccf53f56542e8044289a3ae4ef
freilinjb <freilinjb@gmail.com>
2020-05-16 14:30:49 -0400

    Probando nuevo dineno

--- components/ui/Prestamo.jsx
+++ components/ui/Prestamo.jsx
@@ -5,15 +5,47 @@ import { es } from "date-fns/locale";
 
 import useCalculadora from '../../hooks/useCalculadora';
 
+
+
 const Prestamo = ({prestamo}) => {
+
+  var meses = [
+    "Enero", "Febrero", "Marzo",
+    "Abril", "Mayo", "Junio", "Julio",
+    "Agosto", "Septiembre", "Octubre",
+    "Noviembre", "Diciembre"
+  ]
+
     const { setMoneda } = useCalculadora();
     const estadoCuotas = {
         pendiente: 0,
         atrasados: 0,
         pagados: 0
     }
+    let fecha = {
+      proximoPago: '',
+      dia: '',
+      mes:'',
+      anio: ''
+    };
 
-    const fecha = {fechaInicial:'',fechaFinal: ''}
+    for(const i in prestamo.detallesCuotas) {
+      if(prestamo.detallesCuotas[i].estado === 'pendiente') {
+        fecha.proximoPago = prestamo.detallesCuotas[i].fecha;
+
+        const f = fecha.proximoPago.split('-');
+        fecha.dia = f[0];
+        fecha.mes = f[1];
+        fecha.anio = f[2];
+
+        console.log(fecha);
+        
+        break;
+      }
+    }
+
+    console.log('',meses[Number(fecha.mes)],'mes', fecha.mes);
+    
     for(const i in prestamo.detallesCuotas) {
         if(prestamo.detallesCuotas[i].estado === 'pendiente') {
             estadoCuotas.pendiente++;
@@ -22,62 +54,53 @@ const Prestamo = ({prestamo}) => {
         } else  if(prestamo.detallesCuotas[i].estado === 'pagados') {
             estadoCuotas.pagados++;
         }
-        console.log(prestamo.detallesCuotas[i]);
+        // console.log(prestamo.detallesCuotas[i]);
     }
 
 
     return ( 
         <>
         {/* <!-- Project--> */}
-          <div key={prestamo.id} className="project">
-            <div className="row bg-white has-shadow">
-              <div className="left-col col-lg-6 d-flex align-items-center justify-content-between">
-                <div className="project-title d-flex align-items-center">
-                  <div className="image has-shadow">
-                    <img
-                      src="https://bootdey.com/img/Content/avatar/avatar8.png"
-                      alt="..."
-                      className="img-fluid"
-                    />
-                  </div>
-                  <div className="text">
-                  <Link href="/prestamo/[id]" as={`prestamo/${prestamo.id}`}><a><h3 className="h4">{prestamo.cliente.nombre +' '+prestamo.cliente.apellido}</h3></a></Link>
-                    <p>Prestado: {setMoneda(prestamo.monto)}</p>
-                    <p>Periodo de pagos: {prestamo.periodoPagos}</p>
-                    <p>Tasa de Interes: {prestamo.tasaInteres}%</p>
-                  </div>
-                </div>
-                <div className="project-date">
-                    <span className="">Proximo pago: {prestamo.detallesCuotas[0].fecha}</span>
-                </div>
-              </div>
-              <div className="right-col col-lg-6 d-flex align-items-center">
-                <div className="time">
-                  <i className="fa fa-clock-o"></i>
-                  {formatDistanceToNow(new Date(prestamo.creado), {
-                    locale: es,
-                  })}
-                </div>
-                <div className="comments">
-                  <i className="fa fa-comment-o"></i>
-                  {estadoCuotas.pendiente} / {estadoCuotas.pagados}
-                </div>
-                <div className="project-progress">
-                  <div className="progress">
-                    <div
-                      role="progressbar"
-                      aria-valuenow="25"
-                      aria-valuemin="0"
-                      aria-valuemax="100"
-                      className="progress-bar float-right"
-                    ></div>
-                    <div className="">{prestamo.estado}</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* <!-- Project--> */}
+        <li>
+            <time datetime={fecha.proximoPago}>
+              <span className="day">{fecha.dia}</span>
+							<span className="month">{meses[Number(fecha.mes)]}</span>
+							<span className="year">{fecha.anio}</span>
+						</time>
+            <img alt="My 24th Birthday!" src="https://farm5.staticflickr.com/4150/5045502202_1d867c8a41_q.jpg" />
+						<div className="info">
+							<h2 className="title">{prestamo.cliente.nombre + ' ' + prestamo.cliente.apellido}</h2>
+              <p className="desc">Cuotas: {estadoCuotas.pendiente} / {estadoCuotas.pagados}</p>
+							<ul className="detalles">
+								<li><a href="#website"><span className="fa fa-globe"></span> Pagar</a></li>
+								<li><span className="fa fa-money"></span> {setMoneda(prestamo.monto)}</li>
+							</ul>
+						</div>
+						<div className="social">
+							<ul>
+								<li className="telefono"><a href="#facebook"><span className="fa fa-facebook"></span></a></li>
+								<li className="correo"><a href="#twitter"><span className="fa fa-twitter"></span></a></li>
+								<li className="pagar"><a href="#google-plus"><span className="fa fa-google-plus"></span></a></li>
+                </ul>
+						</div>
+					</li>
+          <style>{`
+              .detalles {
+                width: 50%;
+              }
+
+              .telefono {
+                width: 33%;
+              }
+
+              .correo {
+                width: 34%;
+              }
+
+              .pagar {
+                width: 33%;
+              }
+            `}</style>
         </>
      );
 }
